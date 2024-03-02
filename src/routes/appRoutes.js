650cc0f31bbe1da53cd878f9b9ee8e1c6aa29cdb
Ali Bayat <alibyt21@gmail.com>
2024-03-02 14:16:05 +0330

    formik added

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,15 +1,32 @@
+import { Outlet } from "react-router-dom";
 import ProtectedView from "src/views/ProtectedView";
+import PublicView from "src/views/PublicView";
+import Temp from "src/views/app/Temp";
 import NotFound from "src/views/errors/NotFound";
 
 export const appRoutes = [
   {
     path: "/app/",
-    element: <ProtectedView />,
+    element: <PublicView />,
     errorElement: <NotFound />,
     children: [
       {
         path: "",
-        element: <p>salam</p>,
+        element: <p>app root</p>,
+      },
+      {
+        path: "entity/",
+        element: <Outlet />,
+        children: [
+          {
+            path: "",
+            element: <Temp />
+          },
+          {
+            path: "add",
+            element: <p>/add of entity path</p>
+          }
+        ]
       },
     ],
   },
