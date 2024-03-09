6328acf1f532092060fad57d3337cf2ffd0dda68
Ali Bayat <alibyt21@gmail.com>
2024-03-09 13:21:22 +0330

    home link added to main menu

--- src/components/Sidebar/SidebarMain/SidebarHeader.jsx
+++ src/components/Sidebar/SidebarMain/SidebarHeader.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+import { homeURL } from "src/settings/config";
+
 export default function SidebarHeader() {
     return (
         <div className="flex items-center justify-center flex-shrink-0 p-2">
-            <span className="flex flex-col justify-center items-center p-2 text-lg font-medium leading-8 tracking-wider whitespace-nowrap">
-                <img src="assets/img/iritco.png" className="w-10" />
-                <span>سمین</span>
-            </span>
+            <Link to={homeURL}>
+                <span className="flex flex-col justify-center items-center p-2 text-lg font-medium leading-8 tracking-wider whitespace-nowrap">
+                    <img src="/assets/img/iritco.png" className="w-10" />
+                    <span>سمین</span>
+                </span>
+            </Link>
         </div>
     )
 }
