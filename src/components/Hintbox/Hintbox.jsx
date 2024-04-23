a6bf41fb5cd501dd40b1136a373ac52732144ccc
Ali Bayat <alibyt21@gmail.com>
2024-04-23 10:06:23 +0330

    hintbox component added

---
+++ src/components/Hintbox/Hintbox.jsx
@@ -0,0 +1,37 @@
+import { GoAlertFill } from "react-icons/go";
+import { GoXCircleFill } from "react-icons/go";
+import { GoCheckCircleFill } from "react-icons/go";
+
+export default function Hintbox({ text, type = "alert" }) {
+    switch (type) {
+        case "alert":
+            return (
+                <div className='flex items-center gap-2 border-2 border-dashed border-orange-300 bg-orange-50 rounded-xl p-4'>
+                    <GoAlertFill className='text-orange-300' size={18} />
+                    <span className='text-orange-800'>
+                        {text}
+                    </span>
+                </div>
+            )
+        case "success":
+            return (
+                <div className='flex items-center gap-2 border-2 border-dashed border-green-400 bg-green-50 rounded-xl p-4'>
+                    <GoCheckCircleFill className='text-green-400' size={18} />
+                    <span className='text-green-800'>
+                        {text}
+                    </span>
+                </div>
+            )
+        case "error":
+            return (
+                <div className='flex items-center gap-2 border-2 border-dashed border-red-400 bg-red-50 rounded-xl p-4'>
+                    <GoXCircleFill className='text-red-400' size={18} />
+                    <span className='text-green-800'>
+                        {text}
+                    </span>
+                </div>
+            )
+        default:
+            break;
+    }
+}
