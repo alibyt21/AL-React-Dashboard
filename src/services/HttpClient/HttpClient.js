7599721efe79b6c95a44f1e1d9311bcffefa9f4c
Ali Bayat <alibyt21@gmail.com>
2024-04-27 09:55:35 +0330

    put and remove method added to http client

--- src/services/HttpClient/HttpClient.js
+++ src/services/HttpClient/HttpClient.js
@@ -71,3 +71,51 @@ export async function post(url, data = {}, config = {}) {
             }
         });
 }
+
+export async function put(url, data = {}, config = {}) {
+    return await axios
+      .put(url, data, config)
+      .then((response) => 200)
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  }
+  
+  export async function remove(url, parameters, config = {}) {
+    return await axios
+      .delete(`${url}?id=${parameters}`, config)
+      .then((response) => 200)
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  }
