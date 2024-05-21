ebe65d76f54623959227e1ed55299703e3f14db1
Ali Bayat <alibyt21@gmail.com>
2024-05-21 09:59:45 +0330

    multitab bug fixed

--- src/components/MultiTab/MultiTab.jsx
+++ src/components/MultiTab/MultiTab.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 
-export default function MultiTab({ children }) {
+export default function MultiTab({ children, type = 1 }) {
+    // children always should be an array
+    if (!Array.isArray(children)) {
+        children = [children]
+    }
+
+    children = children.filter(function (single) {
+        return single;
+    })
 
     // set first tab active by default
-    const [activeTab, setActiveTab] = useState(children[0].props.title);
+    const [activeTab, setActiveTab] = useState(children[0]?.props?.title);
 
     const handleTabChange = (tab) => {
         setActiveTab(tab);
@@ -12,20 +20,33 @@ export default function MultiTab({ children }) {
     return (
 
         <div className='flex flex-col gap-3'>
-            <div className='flex'>
+            <div className={`flex ${type != 1 && "gap-2"}`}>
                 {
                     children.map(
                         (child) => {
-                            return (
-                                <button
-                                    onClick={() => handleTabChange(child.props.title)}
-                                    className={`${activeTab === child.props.title ? 'border-gray-300' : 'border-gray-100'} border-solid border-b-2 px-2`}
-                                >
-                                    {
-                                        child.props.title
-                                    }
-                                </button>
-                            )
+                            if (type == 1) {
+                                return (
+                                    <button
+                                        onClick={() => handleTabChange(child?.props?.title)}
+                                        className={`${activeTab === child?.props?.title ? 'border-gray-300' : 'border-gray-100'} border-solid border-b-2 px-2 pb-2`}
+                                    >
+                                        {
+                                            child?.props?.title
+                                        }
+                                    </button>
+                                )
+                            } else {
+                                return (
+                                    <button
+                                        onClick={() => handleTabChange(child?.props?.title)}
+                                        className={`${activeTab === child?.props?.title ? 'border-gray-300' : 'border-white'} border-solid border p-2 rounded-3xl`}
+                                    >
+                                        {
+                                            child?.props?.title
+                                        }
+                                    </button>
+                                )
+                            }
                         }
                     )
                 }
@@ -34,10 +55,10 @@ export default function MultiTab({ children }) {
                 {
                     children.map(
                         (child) => {
-                            if (activeTab === child.props.title) {
+                            if (activeTab === child?.props?.title) {
                                 return (
                                     <div>
-                                        {child.props.children}
+                                        {child?.props.children}
                                     </div>
                                 )
                             }
