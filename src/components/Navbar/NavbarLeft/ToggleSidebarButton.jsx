57842d7563624b943c059c07ce128ea29534fdb5
Ali Bayat <alibyt21@gmail.com>
2024-11-17 10:48:09 +0330

    toggle sidebar button ui bug fixed

--- src/components/Navbar/NavbarLeft/ToggleSidebarButton.jsx
+++ src/components/Navbar/NavbarLeft/ToggleSidebarButton.jsx
@@ -15,7 +15,7 @@ export default function ToggleSidebarButton() {
         style={{ width: "10px" }}
         fill={`${sidebarState != 0 ? "#000066" : "#cecece"}`}
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 9 17"
+        viewBox="0 0 8 17"
       >
         <rect x="0.48" y="0.5" width="7" height="1" />
         <rect x="0.48" y="7.5" width="7" height="1" />
@@ -26,7 +26,7 @@ export default function ToggleSidebarButton() {
         fill={`${sidebarState == 2 ? "#000066" : "#cecece"}`}
         style={{ width: "20px" }}
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 18 17"
+        viewBox="0 0 16 17"
       >
         <rect x="1.56" y="0.5" width="16" height="1" />
         <rect x="1.56" y="7.5" width="16" height="1" />
