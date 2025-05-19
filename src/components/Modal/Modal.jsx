a8d115eccc1eb1a587259d3e8914e160b192ffd4
Ali bayat <alibyt21@gmail.com>
2025-05-19 11:16:52 +0330

    Fixes #5: modal zindex bug fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
-export default function Modal({ isOpen, toggle, children, width = 800, top = 0, left, right }) {
+export default function Modal({ isOpen, toggle = () => { }, children, width = 800, top = 0, left, right }) {
 
     return (
         <>
             <div
-                className={`${isOpen ? "opacity-100 visible" : "opacity-0 invisible"} bg-[#00000055] overflow-auto transition-all duration-300 ease-in-out flex fixed top-0 right-0 left-0 bottom-0 z-50 h-screen w-full p-2`}
-                onClick={toggle}
+                className={`${isOpen ? "opacity-100 visible z-50" : "opacity-0 invisible z-0"} bg-[#00000055] overflow-auto transition-all duration-300 ease-in-out flex fixed top-0 right-0 left-0 bottom-0 h-screen w-full p-2`}
+                onClick={() => toggle(!isOpen)}
             >
                 <div
                     onClick={e => e.stopPropagation()}
-                    className={`${isOpen ? "" : "translate-y-20"} ${(top && left) || (top && right) ? "absolute" : ""} bg-white transition-all duration-300 ease-in-out m-auto w-full shadow-lg rounded`}
+                    className={`${isOpen ? "" : "translate-y-20"} ${(top && left) || (top && right) ? "absolute" : ""} bg-white dark:bg-black transition-all duration-300 ease-in-out m-auto w-full shadow-lg rounded`}
                     style={{
                         maxWidth: `${width}px`,
                         top: `${top}px`,
